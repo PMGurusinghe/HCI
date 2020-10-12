f151799501ecc3fc933dddcb5e4db5b97b293aab
PMGurusinghe <madhushangurusinghe@gmail.com>
2020-10-12 13:11:50 +0530

    .

--- src/Component/Maincomponent.jsx
+++ src/Component/Maincomponent.jsx
@@ -1,22 +1,15 @@
-import React, { Component } from 'react';
-import '../Component/main.css'
-class Maincomponent extends Component {
+import React, { Component } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+export default class Maincomponent extends Component {
     render() {
         return (
             <div>
-              <div className='bodypage'>
-      <div className="container-fluid text-center">
-        <div className="px-5 py-5">
-          <h1 className="not-found">404</h1>
-          <h3>We couldn't seem to find the page you are looking for!</h3>
-          <div className="text-center mt-4 mb-5"> <button className="btn btn-success send px-3"><i className="fa fa-long-arrow-left mr-1" /> Return to main page</button> </div>
-        </div>
-      </div>
-      </div>
+                <h1>Page not found </h1>
+                <span>
+                <FontAwesomeIcon icon='faUser' />
+                </span>
             </div>
-            
-        );
+        )
     }
 }
-
-export default Maincomponent;
--- src/Component/Myaccount/Myaccount.jsx
+++ src/Component/Myaccount/Myaccount.jsx
@@ -1,62 +1,27 @@
-import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
-import Sidenav from './Sidenav/Sidenav'
-import { Switch } from 'react-router-dom';
-import './Myaccount.css'
-import imagepropic from '../images/images.png'
-class Myaccount extends Component {
+import React, { Component } from 'react'
+import "./Myaccount.css"
+
+export default class Myaccount extends Component {
     render() {
         return (
-            <div className='d-flex'>
-
-        <Route>
-             <Switch>
-             <Route exact component={Sidenav}></Route>
-             </Switch>
-         </Route>
-               
-                {/**PRofile details**/}
-                
-                <ul className="nav">
-                    <li className="nav-item"> <div className="box box-widget widget-user">
-                        <div className="widget-user-header bg-aqua-active">
-                            <h3 className="widget-user-username">Alexander Pierce</h3>
-                            <h5 className="widget-user-desc">Founder &amp; CEO</h5>
-                            <div className="widget-user-image"> <img className="img-circle" src={imagepropic} alt="User Avatar" /> </div>
-                        </div>
-                      
-                       
-                        <div className="box-footer">
-                            <div className="row">
-                                <div className="col-sm-4 border-right">
-                                    <div className="description-block">
-                                        <h5 className="description-header">3,200</h5> <span className="description-text">SALES</span>
-                                    </div>
-                                </div>
-                                <div className="col-sm-4 border-right">
-                                    <div className="description-block">
-                                        <h5 className="description-header">13,000</h5> <span className="description-text">FOLLOWERS</span>
-                                    </div>
-                                </div>
-                                <div className="col-sm-4">
-                                    <div className="description-block">
-                                        <h5 className="description-header">35</h5> <span className="description-text">PRODUCTS</span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div></li>
-
-                </ul>
-
+     <div className='myaccount-parent'>
+         <div className='sidebar-nav'>
+            <h2>User Options</h2>
+           </div>
+        <div class="col-4">
+            <div class="list-group" id="list-tab" role="tablist">
+            <a class="list-group-item list-group-item-action active" id="list-home-list" data-toggle="list" href="#list-home" role="tab" aria-controls="home">Home</a>
+            <a class="list-group-item list-group-item-action" id="list-profile-list" data-toggle="list" href="#list-profile" role="tab" aria-controls="profile">Profile</a>
+            <a class="list-group-item list-group-item-action" id="list-messages-list" data-toggle="list" href="#list-messages" role="tab" aria-controls="messages">Messages</a>
+            <a class="list-group-item list-group-item-action" id="list-settings-list" data-toggle="list" href="#list-settings" role="tab" aria-controls="settings">Settings</a>
             </div>
-
-
-
-
-
-        );
+           
+            </div> 
+       
+         <div className="alert alert-primary" role="alert">
+            A simple primary alert—check it out!
+            </div>
+    </div>
+        )
     }
 }
-
-export default Myaccount;
