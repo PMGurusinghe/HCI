af3bdffc1dfc288471cb6ede3397b7e992cbf61b
haritha1012 <haritha981012@gmail.com>
2020-10-16 22:07:39 +0530

    terms push

--- src/Component/Terms/Terms.jsx
+++ src/Component/Terms/Terms.jsx
@@ -3,13 +3,13 @@ import React, { Component } from "react";
 class Terms extends Component {
   render() {
     return (
-      <div className="page-container">
-        <div className="page-header text-center text-dark">
+      <div className="card-container">
+        <div className="card-header text-center" style={{backgroundColor:"gray"}}>
           <h1>Terms of Use</h1>
         </div>
-        <div className="page-content">
-          <p>
-            Terms & Conditions for Websites Because a Terms and Conditions
+        <div className="card text-center">
+          <p className="card-body text-center text-dark" style={{marginLeft:'50px'}}>
+            * Terms & Conditions for Websites Because a Terms and Conditions
             agreement is the agreement where you inform the users of your
             website about the rules, terms and guidelines that they need to
             follow in order to use and access your website, a Terms and
@@ -22,6 +22,20 @@ class Terms extends Component {
             And so on If you don't have this agreement for your website yet, use
             the Generator to create it!
           </p>
+          <p className="card-body text-center text-dark" style={{marginLeft:'50px'}}>
+             * Unlike a Privacy Policy that is required for almost all online businesses, a Terms and Conditions isn't required for an ecommerce store, but it's extremely important to have it.A Terms and Conditions could legally protect your ecommerce store. It is in this type of legal agreement where you set the rules that your customers would follow during a purchase and limit your liability in the event that your products fail.
+          </p>
+            <p className="card-body text-center text-dark" style={{marginLeft:'50px'}}>
+            * The Terms and Conditions agreement can act as a legal contract between you, the mobile app owner or developer, and the users of your app. Like a Terms and Conditions for a website, this agreement for a mobile app would set the rules and terms that users must follow in order to use your app.
+            Here are a couple of reasons why you'll want to have a Terms and Conditions for a mobile app:
+            You can stop abusive users from using your app.
+            You can terminate or block accounts at your sole discretion.
+            Liability to users will be limited.
+            And many more.
+            If you don't have this agreement for your mobile app yet, use the Generator to create it!
+            </p>
+            <p className="card-body text-center text-dark" style={{marginLeft:'50px'}}>A Terms and Conditions URL is asked by the Facebook team when you are submitting your Facebook app for review. While the agreement isn't mandatory by law, Facebook asks for the URL to your Terms and Conditions at the "Contact Info" section.
+               * It's beneficial to have a Terms and Conditions for your app before you submit it to the review process as you can outline what users must do or cannot do to access and continue to access your Facebook app once it is live.</p>
         </div>
       </div>
     );
